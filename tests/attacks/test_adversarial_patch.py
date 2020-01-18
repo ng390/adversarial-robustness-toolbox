b3d3a940f1e183cd6da084821b034c13ded29eda
Beat Buesser <beat.buesser@ie.ibm.com>
2020-01-18 11:42:46 +0000

    Update requirements.txt
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- tests/attacks/test_adversarial_patch.py
+++ tests/attacks/test_adversarial_patch.py
@@ -28,7 +28,7 @@ from art.utils import master_seed
 from art.classifiers.scikitlearn import ScikitlearnDecisionTreeClassifier
 
 from tests.utils_test import TestBase
-from tests.utils_test import get_classifier_tf#, get_classifier_kr, get_classifier_pt, get_iris_classifier_kr
+from tests.utils_test import get_classifier_tf, get_classifier_kr, get_classifier_pt, get_iris_classifier_kr
 # from tests.utils_test import get_classifier_tf, get_classifier_kr#, get_classifier_pt, get_iris_classifier_kr
 
 logger = logging.getLogger(__name__)
